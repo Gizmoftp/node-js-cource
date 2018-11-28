851d00e3b5aad8112654cdcbfe0a767b60521cf9
Gizmoftp <Andrey.Burda@gizmo.ftp@gmail.com>
2018-11-28 13:59:17 +0300

    setup socket io

--- server/server.js
+++ server/server.js
@@ -1,11 +1,26 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
-const app = express();
 const port = process.env.PORT || 3000
 let publicPath = path.join(__dirname, '../public');
+
+const app = express();
+const server = http.createServer(app);
+
+let io = socketIO(server);
+
+io.on('connection', (socket) => {
+console.log('new user connected');
+
+socket.on('disconnect', () => {
+    console.log('The user is disconnected');
+})
+})
+
 
 
 app.use(express.static(publicPath));
 
-app.listen(port, () => { console.log(`Start listening port ${port}`);});
+server.listen(port, () => { console.log(`Start listening port ${port}`);});
